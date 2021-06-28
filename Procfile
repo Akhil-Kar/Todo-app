0a5f61c6b4552e7db6f99d21ad91cd8c3c5a8e2f
Akhil <akhilkar@ternaengg.ac.in>
2021-06-28 09:59:29 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tododjango.wsgi --log-file -
---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', TaskLogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name='logout'),
+    path('register/', TaskRegister.as_view(), name='register'),
+    path('', TaskList.as_view(), name='list'),
+    path('detail/<int:pk>/', TaskDetail.as_view(), name='detail'),
+    path('create/', TaskCreate.as_view(), name='create'),
+    path('edit/<int:pk>/', TaskUpdate.as_view(), name='edit'),
+    path('delete/<int:pk>/', TaskDelete.as_view(), name='delete'),
+]
